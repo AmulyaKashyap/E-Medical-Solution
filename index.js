f30c6505a23fdd32059fec16f2f8271efba6cbc5
AmulyaKashyap <40742391+AmulyaKashyap@users.noreply.github.com>
2021-10-19 16:24:17 +0530

    set up view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port=8000; //80 while deploying
 //use express router
 app.use('/',require('./routes/index'));
 
+//setting up a view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         //console.log('Error: ',err);
