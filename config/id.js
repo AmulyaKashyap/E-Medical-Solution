965554c9723389e413c627e1e51644376b212f9a
Impachouri <87123868+Impachouri@users.noreply.github.com>
2021-10-29 13:52:10 +0530

    Delete id.js

--- config/id.js
+++
@@ -1,4 +0,0 @@
-id : 953138738247-7im47lh42ohvideom2o7204kbau7poht.apps.googleusercontent.com
-
-id : 953138738247-7im47lh42ohvideom2o7204kbau7poht.apps.googleusercontent.com
-secret : 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
